e56dce2aeef6307ffa00bbafdc46515e7aa13106
Stefanos Georgakis <s_georgakis@hotmail.com>
2018-10-06 17:36:01 +0300

    Decouple presentation layer entity

--- src/kafka-consumer/src/main/java/com/pollfish/consumer/web/rest/vm/LoggingEventVM.java
+++ src/kafka-consumer/src/main/java/com/pollfish/consumer/web/rest/vm/LoggingEventVM.java
@@ -1,6 +1,82 @@
 package com.pollfish.consumer.web.rest.vm;
 
-import com.pollfish.consumer.service.dto.LoggingEventDTO;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.pollfish.consumer.service.dto.LevelType;
 
-public class LoggingEventVM extends LoggingEventDTO {
+import java.util.Date;
+
+public class LoggingEventVM {
+
+    private String id;
+
+    private int version;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+    private Date time;
+
+    private String message;
+
+    private LevelType level;
+
+    private int app;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LevelType getLevel() {
+        return level;
+    }
+
+    public void setLevel(LevelType level) {
+        this.level = level;
+    }
+
+    public int getApp() {
+        return app;
+    }
+
+    public void setApp(int app) {
+        this.app = app;
+    }
+
+    @Override
+    public String toString() {
+        return "LoggingEventVM{"
+                + "id='" + id + '\''
+                + ", version=" + version
+                + ", time=" + time
+                + ", message='" + message + '\''
+                + ", level=" + level
+                + ", app=" + app
+                + '}';
+    }
 }
