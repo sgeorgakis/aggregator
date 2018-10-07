994cd73e1962f7fdae676765d8404d735b500207
Stefanos Georgakis <s_georgakis@hotmail.com>
2018-10-07 16:48:34 +0300

    Remove duplicate files

--- lib/docker/docker/cassandra/init-database.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-until cqlsh -f /opt/init-database.cql; do
-  echo "cqlsh: Cassandra is unavailable to initialize - will retry later"
-  sleep 2
-done &
-
-exec /docker-entrypoint.sh "$@"
